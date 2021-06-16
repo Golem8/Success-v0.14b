e0c10bafa6cb8a208b53062550552e6d901492d8
Simon <sjd3002@gmail.com>
2021-06-16 12:16:39 -0600

    added imdad responder

---
+++ responders/imdad.js
@@ -0,0 +1,26 @@
+module.exports = {
+	name: 'imdad',
+	description: 'When a user says I\'m _, the bot replies "hi _, I\'m dad"',
+	guildOnly: false,
+	aliases: [],
+	cooldown: 0,
+	argsRequired: false,
+
+	async execute(message) {
+        const niceMessage = message.content.toLowerCase();
+
+        //checks for i'm
+        const scan = niceMessage.includes("i'm");
+        if ( scan ){
+            const word = niceMessage.slice(niceMessage.indexOf("i'm") + "i'm".length);
+            message.reply(`Hi${word}, I'm dad!`)
+        }
+
+        //checks for im
+        const scan2 = niceMessage.includes("im");
+        if ( scan2 ){
+            const word = niceMessage.slice(niceMessage.indexOf("im") + "im".length);
+            message.reply(`Hi${word}, I'm dad!`)
+        }
+	},
+};
