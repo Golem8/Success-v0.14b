d6d9f971cb24a17b4364546262459006d52e7f3f
TheE1337 <xkcdblag@gmail.com>
2021-06-17 13:36:42 -0600

    Update imdad.js
    Now trims to guarantee exactly one space after I'm in the response and more robustly recognizes I'm and im at the start of messages.

--- responders/imdad.js
+++ responders/imdad.js
@@ -10,17 +10,17 @@ module.exports = {
         const niceMessage = message.content.toLowerCase();
 
         //checks for i'm
-        const scan = niceMessage.includes(" i'm ");
+        const scan = (niceMessage.includes(" i'm ") || (niceMessage.length >= 4 && niceMessage.substring(0,2) === "i'm"));
         if ( scan ){
-            const word = niceMessage.slice(niceMessage.indexOf(" i'm ") + " i'm ".length);
-            message.reply(`Hi${word}, I'm dad!`)
+            const word = niceMessage.slice(niceMessage.indexOf(" i'm ") + " i'm ".length).trim();
+            message.reply(`Hi ${word}, I'm dad!`)
         }
 
         //checks for im
-        const scan2 = niceMessage.includes(" im ");
+        const scan2 = niceMessage.includes(" im ") || (niceMessage.length >= 3 && niceMessage.substring(0,1) === "im");
         if ( scan2 ){
-            const word = niceMessage.slice(niceMessage.indexOf(" im ") + " im ".length);
-            message.reply(`Hi${word}, I'm dad!`)
+            const word = niceMessage.slice(niceMessage.indexOf(" im ") + " im ".length).trim();
+            message.reply(`Hi ${word}, I'm dad!`)
         }
 	},
 };
