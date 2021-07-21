5bdd2d29fbd8d16426ec3138e948321058011285
Simon Dick <sjd3002@gmail.com>
2021-07-21 11:48:00 -0600

    mid-word im's wont trigger anymore

--- responders/imdad.js
+++ responders/imdad.js
@@ -9,18 +9,32 @@ module.exports = {
 	async execute(message) {
         const niceMessage = message.content.toLowerCase();
 
-        //checks for i'm
-        var scan = niceMessage.includes("i'm");
+        var scan = niceMessage.startsWith("i'm ");
         if ( scan ){
             const word = niceMessage.slice(niceMessage.indexOf("i'm") + "i'm".length);
             if (word != '')
                 message.reply(`Hi ${word.trim()}, I'm dad!`)
         }
 
-        //checks for im
-        scan = niceMessage.includes("im");
+        scan = niceMessage.startsWith("im ");
         if ( scan ){
             const word = niceMessage.slice(niceMessage.indexOf("im") + "im".length);
+            if (word != '')
+                message.reply(`Hi ${word.trim()}, I'm dad!`)
+        }
+
+        //checks for i'm
+        var scan = niceMessage.includes(" i'm");
+        if ( scan ){
+            const word = niceMessage.slice(niceMessage.indexOf(" i'm") + " i'm".length);
+            if (word != '')
+                message.reply(`Hi ${word.trim()}, I'm dad!`)
+        }
+
+        //checks for im
+        scan = niceMessage.includes(" im");
+        if ( scan ){
+            const word = niceMessage.slice(niceMessage.indexOf(" im") + " im".length);
             if (word != '')
                 message.reply(`Hi ${word.trim()}, I'm dad!`)
         }
