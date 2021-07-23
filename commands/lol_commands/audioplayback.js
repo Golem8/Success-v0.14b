ed7cc241c213c30338fc5fdacdfee774acf1f519
Simon Dick <sjd3002@gmail.com>
2021-07-23 17:03:36 -0600

    bot shouldnt listen to itself

--- commands/lol_commands/audioplayback.js
+++ commands/lol_commands/audioplayback.js
@@ -9,9 +9,11 @@ module.exports = {
 	async execute(message, args) {
         if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
-                message.member.voice.channel.members.forEach((member) => { 
-                    const audio = connection.receiver.createStream(member.user.id, { end: 'manual' });
-                    connection.play(audio, { type: 'opus', volume: 3 });
+                message.member.voice.channel.members.forEach((member) => {
+                    if (member.user.id != message.client.user.id){
+                        const audio = connection.receiver.createStream(member.user.id, { end: 'manual' });
+                        connection.play(audio, { type: 'opus', volume: 3 });
+                    }
                 });
             
         } else {
