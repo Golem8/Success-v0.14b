3153b72ccfd514eeee182c017c88e62e53a83f2c
Simon Dick <sjd3002@gmail.com>
2021-10-05 20:32:54 -0400

    update delayed strike

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ client.on('presenceUpdate', (oldMember, newMember) => {
 
 // checks for
 client.on('messageUpdate', (oldMessage, newMessage) => {
-	if (Math.random() <= 0.5){
+	if (Math.random() <= 0.99){
     setTimeout(function(){ 
       if(Boolean(Math.round(Math.random()))){
         newMessage.channel.send(`<@!${newMessage.author.id}>, your treason has not gone unnoticed.`);
