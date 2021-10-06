56c564f37a5b977e5eba045f2c92d84f626337ab
Simon Dick <sjd3002@gmail.com>
2021-10-06 12:17:30 -0400

    fishhunt

---
+++ commands/lol_commands/fishhunt.js
@@ -0,0 +1,51 @@
+module.exports = {
+	name: 'hunt',
+	description: 'FISH HUNT',
+	guildOnly: true,
+	argsRequired: false,
+
+	async execute(message, args) {
+        for (let index = 0; index < 3; index++) {
+            message.channel.send('YO');  
+        }
+        message.channel.send("It's a perfect day for a fish hunt");
+        message.channel.send("So I'll load up all my swords in his Honda Accord Sport");
+        message.channel.send("**2015**");
+        message.channel.send("it's a good car");
+        message.channel.send("Not a great car, but it's a good car");
+        message.channel.send("Electric windows down, feel the breeze");
+        message.channel.send("I brought **CDs!**");
+        message.channel.send("Each one's Alicia Keys");
+        message.channel.send("We get pumped 'n ready");
+        message.channel.send("I got guns aplenty");
+        message.channel.send("Man, these fish gonna die");
+        message.channel.send("Lock hands 'till they get sweaty!");
+        message.channel.send("And when they dry, then we know we arrived");
+        message.channel.send("Grab the fish killing permit from the Ranger Guy");
+        message.channel.send("He said, \"Son, listen here, look me in my eye,\"");
+        message.channel.send("He only had one eye! It wasn't hard to find");
+        message.channel.send("I had astigmatism in the left one");
+        message.channel.send("And the fish knew this, so they plucked out my best one");
+        message.channel.send("Not only that, but they killed my wife");
+        message.channel.send("So take this book of spells, and this enchanted scythe.");
+        message.channel.send("**Right**");
+        message.channel.send("Yeah, fish hunt, fish hunt");
+        message.channel.send("These fish have legs, And these fish can run");
+        message.channel.send("Seven foot tall and proficient at fencin' and wrestlin'");
+        message.channel.send("Let's find a nest, and eject them!");
+        message.channel.send("Blood moon rise when we reached the hive");
+        message.channel.send("I was killing many fish with my many knives!");
+        message.channel.send("Shot my musket at the Fish King, one of his fish wings");
+        message.channel.send("He summoned several skeletons to do his bidding!");
+        message.channel.send("Epic fight!");
+        message.channel.send("Too expensive to show");
+        message.channel.send("I'm only one man and I live at home");
+        message.channel.send("But legend has it to this very day");
+        message.channel.send("That the war rages on");
+        message.channel.send("'Tween the brothers and the Fish King");
+        message.channel.send("But that's a story for another time");
+        message.channel.send("So good night little one, dream of dolphins tonight");
+        message.channel.send("That's a story for another time");
+        message.channel.send("So good night little one, dream of dolphins tonight");
+	},
+};
