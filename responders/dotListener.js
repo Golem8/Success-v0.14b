4300b3b33265c84eb0260a591b230612cff702e6
Simon Dick <sjd3002@gmail.com>
2021-10-19 22:58:40 -0400

    fix more bug

--- responders/dotListener.js
+++ responders/dotListener.js
@@ -1,20 +1,23 @@
 const DotCommands = require('../db').DotCommands;
 
 module.exports = {
-	name: 'dotListener',
-	description: 'listens and triggers dot commands',
+    name: 'dotListener',
+    description: 'listens and triggers dot commands',
 
-    async execute(message){
+    async execute(message) {
 
         //get all commands for the guild
-        const cmds = await DotCommands.findAll({ where: { serverid: message.guild.id } });
-        if (cmds == undefined) return;
-        
-        //looping through each command
-        cmds.forEach(cmd => {
-            if (message.content.includes((cmd.get('dotname')))){
-                message.channel.send(cmd.get('cmdoutput'));
-            }
-        });
-      }
+        if (!message.guild === null) { //only check for guilds
+
+            const cmds = await DotCommands.findAll({ where: { serverid: message.guild.id } });
+            if (cmds == undefined) return;
+
+            //looping through each command
+            cmds.forEach(cmd => {
+                if (message.content.includes((cmd.get('dotname')))) {
+                    message.channel.send(cmd.get('cmdoutput'));
+                }
+            });
+        }
+    }
 };
