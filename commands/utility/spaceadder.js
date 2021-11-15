1b3847a792a7a004823d098299ab3044ea642ca9
Simon D <sjd3002@gmail.com>
2021-11-15 17:11:13 -0500

    space adder

---
+++ commands/utility/spaceadder.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: 'spaceadder',
+    description: 'a  d  d  s     s  p  a  c  e  s',
+    usage: 'spaceadder <numspaces> <text>',
+    guildOnly: false,
+    argsRequired: true,
+
+    async execute(message, args) {
+        numSpaces = args[0];
+        args.shift();
+        content = args.join(' ');
+        returnStr = ' '
+
+        if (numSpaces == -1) {
+            for (let i = 0; i < content.length; i++) {
+                returnStr += content.charAt(i);
+                for (let index = 0; index < i; index++) {
+                    returnStr += ' '; //add i spaces
+                }
+            }
+        } else {
+            for (let i = 0; i < content.length; i++) {
+                returnStr += content.charAt(i);
+                for (let index = 0; index < numSpaces; index++) {
+                    returnStr += ' '; //add numSpaces spaces
+                }
+            }
+        }
+        return message.reply(returnStr)
+    },
+};
